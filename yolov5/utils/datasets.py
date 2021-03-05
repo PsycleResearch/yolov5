8a3d1c7f9f0b838d0760bf5d4913d2b240170e3e
Arunavameister <arunavameister@yahoo.com>
2021-03-05 19:53:21 +0100

    Remove annotations images replace.

--- yolov5/utils/datasets.py
+++ yolov5/utils/datasets.py
@@ -331,7 +331,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.label_files = [
             f"{x.replace('images', 'annotations')}.txt"
             if len(x.split(".")) == 1  # If there's no image extension
-            else x.replace('images', 'annotations').replace(x.split(".")[-1], 'txt')  # If there's an image extension
+            else x.replace(x.split(".")[-1], 'txt')  # If there's an image extension
             for x in self.img_files
         ]
 
