744ccb15f182e2fe47098ae27b0d510d3789d916
fabien-lionti <fabien.lionti@gmail.com>
2021-03-15 16:00:40 +0100

    fix

--- repsycle_yolov5/dataset.py
+++ repsycle_yolov5/dataset.py
@@ -23,7 +23,7 @@ class YoloDataset(Dataset):
         self.image_id = list(self.datas.keys())
         self.annotations = list(self.datas.values())
 
-        self.anchors  = torch.tensor(anchors[0] + anchors[1] +anchors[2])
+        self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2])
         self.nb_anchors = self.anchors.shape[0]
         self.C = C
         self.S = S
@@ -34,15 +34,20 @@ class YoloDataset(Dataset):
         return len(self.annotations)
 
     def __getitem__(self, idx):
+
         img_path = img_dir + self.image_id[idx] + '.jpeg'
         bboxes = self.annotations[idx] # [x, y, w, h, class]
         image = Image.open(img_path).convert("RGB")
 
-        targets = [torch.zeros(self.nb_anchors // 3, 3, S, S, 6) for S in self.S] #[prob, x, y, w, h, c]
+        targets = [torch.zeros(self.nb_anchors // 3, S, S, 6) for S in self.S] #[prob, x, y, w, h, c]
 
         # Choose which anchors is responsible for each cells following highest IOU
         for bboxe in bboxes:
-            iou_anchors = iou(torch.tensor(bboxe[2:4]), self.anchors)
+
+            if bboxe is None:
+                continue
+
+            iou_anchors = iou(torch.tensor(bboxe[2:4]), self.anchors) # Get IOU for each anchors
             anchor_indices = iou_anchors.argsort(descending=True, dim=0)
             x, y, width, height, class_label = bboxe
             has_anchor = [False] * 3
@@ -59,29 +64,33 @@ class YoloDataset(Dataset):
                 anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
 
                 if not anchor_taken and has_anchor[scale_idx]:
-                    targets[scale_idx][anchor_on_scale, i, j , 0] = 1
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = 1
                     x_cell, y_cell = S*x - j, S*i - i
-                    width_cell, height_cell = (width * S,
-                                              height * S)
+                    width_cell, height_cell = width * S, height * S
 
                     box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
                     targets[scale_idx][anchor_on_scale, i, j, 1:5]= box_coordinates
                     targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_label)
 
                 elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_tresh:
-                    None
+                  targets[scale_idx][anchor_on_scale, i, j, 0] = -1  # ignore prediction
 
         return image, targets
 
 if __name__ == '__main__':
 
     img_dir = './images/'
-    labels = './datas/val_metadatas.json'
+    labels = './datas/labels.json'
     image_size = (0, 0)
-    anchors = [[(10, 13), (16, 30), (33, 23)],
-                        [(30, 61), (62, 45), (59, 119)],
-                        [(116, 90), (156, 198), (373, 326)]]
+
+    anchors = [
+        [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
+        [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
+        [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
+    ]
+
     ds = YoloDataset(img_dir, labels, anchors, image_size)
 
     for i in range(len(ds)):
-        print(ds[i])
+        target = ds[i][1]
+        None
