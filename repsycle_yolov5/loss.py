f6d9c761b1d43e53d9737210be144b417f16006e
fabien-lionti <fabien.lionti@gmail.com>
2021-03-15 16:07:02 +0100

    loss implementation

---
+++ repsycle_yolov5/loss.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import torch
+from utils import intersection_over_union
+
+class Loss(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.bce = nn.BCEWithLogitsLoss()
+        self.entropy = nn.CrossEntropyLoss()
+        self.sigmoid = nn.Sigmoid()
+
+        self.lambda_class = 1
+        self.lambda_noobj = 10
+        self.lambda_obj = 1
+        self.lambda_bbox = 10
+
+    def forward(self, predictions, target, anchors):
+        pass
