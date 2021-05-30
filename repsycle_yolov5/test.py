36592e3651f858f626e3b2720c8f1b359678a36a
fabien-lionti <fabien.lionti@gmail.com>
2021-05-30 11:43:04 +0200

    ok

--- repsycle_yolov5/test.py
+++ repsycle_yolov5/test.py
@@ -57,15 +57,15 @@ def test(model):
                 for j in range(y_[p].shape[-2]):
                     for s in range(3):
                         objectness = torch.sigmoid(y_[p][0,s,i,j,4]).data.tolist()
-                        # if objectness > 0.5:
-                        x = ( ( j + y_[p][0,s,i,j,0].sigmoid().detach() ) / y_[p].shape[-2] ).data.tolist()
-                        y = ( ( i + y_[p][0,s,i,j,1].sigmoid().detach() ) / y_[p].shape[-2] ).data.tolist()
-                        w = ( (y_[p][0,s,i,j,2].exp().detach()) * scaled_anchors[p][s][0] / y_[p].shape[-2] ).data.tolist()
-                        h = ( (y_[p][0,s,i,j,3].exp().detach()) * scaled_anchors[p][s][1] / y_[p].shape[-2] ).data.tolist()
-                        c = ( torch.argmax(y_[p][0,s,i,j,5:]).detach() ).data.tolist()
-                        predictions.append([x, y, w, h, objectness, c])
+                        if objectness > 0.5:
+                            x = ( ( j + y_[p][0,s,i,j,0].sigmoid().detach() ) / y_[p].shape[-2] ).data.tolist()
+                            y = ( ( i + y_[p][0,s,i,j,1].sigmoid().detach() ) / y_[p].shape[-2] ).data.tolist()
+                            w = ( (y_[p][0,s,i,j,2].exp().detach()) * scaled_anchors[p][s][0] / y_[p].shape[-2] ).data.tolist()
+                            h = ( (y_[p][0,s,i,j,3].exp().detach()) * scaled_anchors[p][s][1] / y_[p].shape[-2] ).data.tolist()
+                            c = ( torch.argmax(y_[p][0,s,i,j,5:]).detach() ).data.tolist()
+                            predictions.append([x, y, w, h, objectness, c])
 
-        predictions = non_max_suppression(predictions, iou_threshold = 0.6, threshold = 0.45)
+        # predictions = non_max_suppression(predictions, iou_threshold = 0.6, threshold = 0.45)
         print(predictions)
         plot_images(image, predictions)
 
